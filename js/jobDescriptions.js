7f89e9f348be89869b340df5965ef1ec5286da6a
Vamsee Kethineni <vamsee_kethineni@scc.senate.gov>
2020-01-28 20:29:54 -0500

    update with careers section

---
+++ js/jobDescriptions.js
@@ -0,0 +1,7 @@
+
+var targetSection = document.getElementById('partials');
+
+function load_content ( id, content ) {
+  var node = document.getElementById( id );
+  node.innerHTML = '../fullStackEngineer.html';
+}
